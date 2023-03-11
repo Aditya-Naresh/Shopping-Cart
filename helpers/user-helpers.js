a2d623bf6279d22898384bdc5328d87377d14e5e
Aditya-Naresh <adityanaresh007@gmail.com>
2023-03-11 12:58:23 +0530

    Login setup completed. Moving on to Session set up

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -11,5 +11,31 @@ module.exports = {
             })
         })
 
+    },
+
+    doLogin: (userData) => {
+        return new Promise(async(resolve,reject) => {
+            let loginStatus = false
+            let response = {}
+            let user = await db.get().collection(collection.USER_COLLECTION).findOne({Email:userData.Email})
+            if(user){
+                 bcrypt.compare(userData.Password,user.Password).then((status)=>{
+                    if(status){
+                        console.log("Login Sucess");
+                        response.user=user
+                        response.status=true
+                        resolve(response)
+                    }else{
+                        console.log("Login failed");
+                        resolve({status:false})
+                    }
+                 })
+            }else{
+                console.log("User doesnt exist");
+                resolve({status:false})
+            }
+        })
     }
+
+
 }
